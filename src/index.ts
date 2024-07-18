d69c8075dd844fd9876b1d24439e532467e3d441
Sujal Pahwa <142010621+sujalpahwa30@users.noreply.github.com>
2024-07-18 17:09:47 +0530

    Added eslint configurations

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express , {Express} from "express";
+
+import express, { Express } from "express";
 import serverConfig from "./config/serverConfig";
 
-const app : Express = express();
 
-app.listen(serverConfig.PORT , () => {
-    console.log(`Server started at *:${serverConfig.PORT}`);
+const app: Express = express();
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server started at *:${serverConfig.PORT}`);
 });
